4af249e0d45db691343094cfd6c7d9ca477e6750
judyliu2 <jliu15@stuy.edu>
2017-02-13 22:17:45 -0500

    heading

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,8 +1,13 @@
+//Jason Lam, Piotr Milewski, Judy Liu
+//APCS2 pd5
+//HW7: What Does the Data Say?
+//2017-02-13
+
 /*======================================
   class MergeSortTester
   ALGORITHM:
   <INSERT YOUR DISTILLATION OF ALGO HERE>
-  BIG-OH CLASSIFICATION OF ALGORITHM:
+  BIG-OH CLASSIFICATION OF ALGORITHM: O(nlog(n))
   <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
